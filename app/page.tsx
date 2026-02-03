2f701a008ebb5bcf82f8ff1d21fa3af6f7760c4b
ralph <jfgcaspe@gmail.com>
2026-02-03 17:34:53 +0800

    Add new Features and Change the theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,22 @@
+import FAQsThree from "@/components/faqs-3";
+import FeaturesSection from "@/components/features-8";
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import IntegrationsSection from "@/components/integrations-5";
+import StatsSection from "@/components/stats-4";
+import TeamSection from "@/components/team";
+
 export default function Home() {
-  return <div>helloguys</div>;
+  return (
+  <div>
+    <HeroSection />
+    <FeaturesSection />
+    <IntegrationsSection />
+    <StatsSection />
+    <TeamSection />
+    <FAQsThree />
+    <FooterSection />
+    </div>
+  );
 }
 
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns:  [
+      {
+      hostname: "ik.imagekit.io",
+      protocol: "https",
+      },
+    ],
+  },
   /* config options here */
 };
 
